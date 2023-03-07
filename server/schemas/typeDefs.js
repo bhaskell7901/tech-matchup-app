00d69554f8cbb3c53a9d1c0ccb11339c3246f77b
Srinithi Ravichandran <emailtonithi@gmail.com>
2023-03-07 10:59:57 -0800

    typedef and resolvers

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,37 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type Tech {
+    _id : ID
+    name : String
+}
+
+type Matchup {
+    _id :ID
+    tech1: String
+    tech2: String
+    tech1_votes:  Int
+    tech2_votes: Int
+
+}
+
+type Query {
+    getAllTech : [Tech]
+    getAllMatchups : [Matchup]
+    getMatchup(matchupId : ID!) : Matchup
+}
+
+type Mutation {
+    createMatchup(tech1: String!,
+        tech2: String!):  Matchup
+    
+    createVote(matchupId : ID!) : Matchup 
+}
+`
+module.exports = typeDefs;
+
+
+// type Auth {
+//     token: ID!
+//     user: User
+//   }
